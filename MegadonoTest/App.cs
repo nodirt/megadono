3423e321703c6b9d227bff490a0f001f14e518c3
nodirt <COMPONENTONE\tnodir>
2013-06-02 20:36:51 -0400

    v1.0

---
+++ MegadonoTest/App.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MegadonoTest
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    partial class App : Application
+    {
+        public static new MainWindow MainWindow
+        {
+            get { return (MainWindow)Application.Current.MainWindow; }
+        }
+        public static new App Current
+        {
+            get { return (App)Application.Current; }
+        }
+
+        public QuestionStorage QuestionStorage { get; private set; }
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            QuestionStorage = new QuestionStorage();
+            try
+            {
+                QuestionStorage.Load();
+            }
+            catch (Exception ex)
+            {
+                MainWindow.Content = new ErrorView(ex.Message);
+            }
+
+            base.OnStartup(e);
+        }
+
+        [STAThread]
+        static void Main()
+        {
+            try
+            {
+                App app = new App();
+                app.StartupUri = new System.Uri("MainWindow.xaml", System.UriKind.Relative);
+                app.Run();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ппц!");
+            }
+        }
+    }
+}
---
+++ MegadonoTest/QuestionParser.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MegadonoTest
+{
+    class QuestionParser
+    {
+        TextReader _input;
+
+        string _curLine;
+        bool ReadLine()
+        {
+            do
+            {
+                _curLine = _input.ReadLine();
+            } while (_curLine != null && _curLine.StartsWith("#"));
+            return _curLine != null;
+        }
+        Question ReadQuestion()
+        {
+            if (string.IsNullOrWhiteSpace(_curLine))
+                return null;
+
+            var question = new Question { Text = _curLine.Trim() };
+            if (question.Text.EndsWith(":"))
+            {
+                question.Text = question.Text.Substring(0, question.Text.Length - 1);
+            }
+
+            try {
+
+                if (!ReadLine())
+                    return null;
+
+                int[] nums = _curLine.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                System.Diagnostics.Debug.Assert(nums.Length >= 4);
+                question.Index = nums[0] + 1;
+                int answerCount = nums[1];
+                question.PointPerAnswer = nums[2];
+                int correctAnswerCount = nums[3];
+                int[] correctAnswers = nums.Skip(4).ToArray();
+
+                while (ReadLine() && !string.IsNullOrWhiteSpace(_curLine))
+                {
+                    question.Answers.Add(new Answer
+                    {
+                        Text = _curLine,
+                        IsCorrect = correctAnswers.Contains(question.Answers.Count + 1)
+                    });
+                }
+
+                if (question.Answers.Count != answerCount || correctAnswerCount != question.CorrectAnswerCount)
+                {
+                    var msg = new StringBuilder();
+                    IEnumerable<Answer> answers;
+                    if (question.Answers.Count != answerCount)
+                    {
+                        msg.AppendFormat("Вроде ответов должно быть {0}, а я нашел {1}", answerCount, question.Answers.Count);
+                        answers = question.Answers;
+                    }
+                    else
+                    {
+                        msg.AppendFormat("Вроде правильных ответов должно быть {0}, а я нашел {1}", correctAnswerCount, question.CorrectAnswerCount);
+                        answers = question.Answers.Where(a => a.IsCorrect);
+                    }
+                    msg.AppendLine(":");
+                    foreach (var ans in answers)
+                    {
+                        msg.AppendLine(ans.Text);
+                    }
+
+                    throw new ApplicationException(msg.ToString().Trim());
+                }
+
+                return question;
+            }
+            catch(Exception ex) {
+                var msg = new StringBuilder();
+                msg.AppendFormat("Чё-то не так с вопросом '{1}'", question.Index, question.Text).AppendLine();
+                msg.Append("Мне не нравится, что ").Append(ex.Message);
+                throw new ApplicationException(msg.ToString());
+            }
+        }
+        public List<Question> ReadQuestions()
+        {
+            var questions = new List<Question>();
+
+            while (ReadLine())
+            {
+                var question = ReadQuestion();
+                if (question == null)
+                    throw new ApplicationException("Cannot process: " + _curLine);
+
+                questions.Add(question);
+                continue;
+            }
+
+            return questions;
+        }
+
+        public List<Question> Parse(TextReader input)
+        {
+            if (input == null)
+                throw new ArgumentNullException("input");
+            _input = input;
+            try
+            {
+                return ReadQuestions();
+            }
+            finally
+            {
+                _input = null;
+            }
+        }
+    }
+}
